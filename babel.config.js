d783b5556d0d4076929c338540424be009b4df5c
prathusingh <prathusingh21@gmail.com>
2022-03-14 20:58:39 -0400

    chore: configure babel for ts aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-}
+const presets = ["module:metro-react-native-babel-preset"]
+
+const plugins = [
+  // the relay plugin should run before other plugins or presets
+  // to ensure the graphql template literals are correctly transformed
+  "relay",
+  [
+    "module-resolver",
+    {
+      root: ["./"],
+      alias: {
+        "@Scenes": "./src/Scenes",
+        "@store": "./src/store",
+        "@helpers": "./src/helpers",
+      },
+    },
+  ],
+]
+
+module.exports = { presets, plugins }
