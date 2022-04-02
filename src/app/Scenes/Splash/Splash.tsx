c498747eea6b65e04d4e0f71cc41fa09b05f59c8
prathusingh <prathusingh21@gmail.com>
2022-04-02 17:31:15 -0400

    fixes

--- src/app/Scenes/Splash/Splash.tsx
+++ src/app/Scenes/Splash/Splash.tsx
@@ -1,1 +1,13 @@
-export {}
+import React from "react"
+import { View } from "react-native"
+import SplashIcon from "app/icons/SplashIcon"
+
+const Splash = () => {
+  return (
+    <View>
+      <SplashIcon />
+    </View>
+  )
+}
+
+export default Splash
