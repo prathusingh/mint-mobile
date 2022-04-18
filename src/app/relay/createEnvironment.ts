364306e7385dc9b473643fe7b251e9a5048abaf6
prathusingh <prathusingh21@gmail.com>
2022-04-18 16:52:26 -0400

    fix: network

--- src/app/relay/createEnvironment.ts
+++ src/app/relay/createEnvironment.ts
@@ -1,3 +1,10 @@
-const createEnvironment = () => {}
+import { Environment, RecordSource, Store } from "relay-runtime"
+const createEnvironment = () => {
+  const source = new RecordSource()
+  const store = new Store(source)
+  return new Environment({
+    store,
+  })
+}
 
 export const defaultEnvironment = createEnvironment()
