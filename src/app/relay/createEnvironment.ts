a8975f133f9905a410de1a8fb101e42ba028ffe0
prathusingh <prathusingh21@gmail.com>
2022-04-30 17:18:04 -0400

    chore: created default environment for relay

--- src/app/relay/createEnvironment.ts
+++ src/app/relay/createEnvironment.ts
@@ -1,10 +1,33 @@
 import { Environment, RecordSource, Store } from "relay-runtime"
-const createEnvironment = () => {
-  const source = new RecordSource()
-  const store = new Store(source)
-  return new Environment({
-    store,
-  })
-}
+import {
+  cacheMiddleware,
+  errorMiddleware,
+  loggerMiddleware,
+  perfMiddleware,
+  RelayNetworkLayer,
+  authMiddleware,
+} from "react-relay-network-modern/node8"
 
-export const defaultEnvironment = createEnvironment()
+const network = new RelayNetworkLayer(
+  [
+    // Default to size 100 and ttl 900000 (15 minutes)
+    cacheMiddleware({
+      size: 100, // max 100 requests
+      ttl: 900000, // 15 minutes
+    }),
+    __DEV__ ? loggerMiddleware() : null,
+    __DEV__ ? errorMiddleware() : null,
+    __DEV__ ? perfMiddleware() : null,
+    authMiddleware(),
+  ],
+  {
+    // `noThrow` is currently marked as "experimental" and may be deprecated in the future.
+    // See: https://github.com/relay-tools/react-relay-network-modern#advanced-options-2nd-argument-after-middlewares
+    noThrow: true,
+  }
+) // as second arg you may pass advanced options for RRNL
+
+export const defaultEnvironment = new Environment({
+  network: network,
+  store: new Store(new RecordSource()),
+})
