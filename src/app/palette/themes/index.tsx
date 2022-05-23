a02889eaefea86e4a596a2614e1eb837e4dcddd6
prathusingh <prathusingh21@gmail.com>
2022-05-22 20:50:37 -0400

    fix: Theme

---
+++ src/app/palette/themes/index.tsx
@@ -0,0 +1,36 @@
+export const THEME = {
+  colors: {
+    /** Full black, primary brand color  */
+    black100: "#000000",
+    /** 60% black, bold copy, lower in hierarchy  */
+    black60: "#707070",
+    /** 30 black (dark grey), placeholder text only  */
+    black30: "#C2C2C2",
+    /** 15 black (grey), borders, divider lines, and grey button only */
+    black15: "#D8D8D8",
+    /** 10 black (grey), borders, divider lines, and grey button only */
+    black10: "#E7E7E7",
+    /** 5 black (light grey), backgrounds only */
+    black5: "#F7F7F7",
+    /** Full Blue. Calls to action, highlights, edits */
+    blue100: "#1023D7",
+    /** Alias of blue100 */
+    brand: "#1023D7",
+    /** 10% of blue100 on white. Backgrounds */
+    blue10: "#E6E7F5",
+    /** Full copper. In consideration, transition, temporary */
+    copper100: "#7B5927",
+    /** 10% of copper100 on white. Backgrounds */
+    copper10: "#ECE9E3",
+    /** Full green. Success, approval, go */
+    green100: "#127438",
+    /** 10% of green100 on white. Backgrounds */
+    green10: "#E0EAE4",
+    /** Full red. Notification, error, stop */
+    red100: "#C82400",
+    /** 10% of red100 on white. Backgrounds */
+    red10: "#F4E4E3",
+    /** Full white */
+    white100: "#FFFFFF",
+  },
+}
