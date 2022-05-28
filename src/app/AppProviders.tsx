aac66bfbe4dff0094900677d9c4b735d1bd402f6
prathusingh <prathusingh21@gmail.com>
2022-05-28 18:34:30 -0400

    feat: added logging

--- src/app/AppProviders.tsx
+++ src/app/AppProviders.tsx
@@ -1,9 +1,12 @@
 import React, { ReactNode } from "react"
+import { LogBox } from "react-native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { NavigationContainer } from "@react-navigation/native"
 import { RelayEnvironmentProvider } from "react-relay/hooks"
 import { defaultEnvironment } from "./relay/createEnvironment"
 import { Theme } from "palette"
+
+LogBox.ignoreLogs(["Expected style "])
 
 export const AppProviders = ({ children }: { children: ReactNode }) => (
   <RelayEnvironmentProvider environment={defaultEnvironment}>
