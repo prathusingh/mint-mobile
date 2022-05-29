794d7f4f51ac22ff67ea2eff499e354194217b18
prathusingh <prathusingh21@gmail.com>
2022-05-29 06:14:47 -0400

    feat: updated theme

--- src/app/palette/Theme.tsx
+++ src/app/palette/Theme.tsx
@@ -1,1 +1,9 @@
-export const Theme = () => {}
+import React from "react"
+import { ThemeProvider } from "styled-components/native"
+import THEME from "./themes"
+
+export const Theme: React.FC<{
+  theme?: keyof typeof THEME
+}> = ({ children, theme = THEME }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
--- src/app/palette/themes/index.ts
+++ src/app/palette/themes/index.ts
@@ -1,3 +1,5 @@
 import { THEME } from "./v1"
 
+export default THEME
+
 export type Color = keyof typeof THEME["colors"]
