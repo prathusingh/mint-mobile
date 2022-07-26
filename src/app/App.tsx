318a2a2accea9cd43c6df09b604f61ababa03996
prathusingh <prathusingh21@gmail.com>
2022-07-26 06:11:07 -0400

    chore: added logic to display the hydrated state

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,14 @@
 import React from "react"
 import { AppProviders } from "./AppProviders"
 import { NavStack } from "./navigation/NavStack"
+import { GlobalStore } from "./store/GlobalStore"
 
 const Main: React.FC = () => {
+  const isHydrated = GlobalStore.useAppState((state) => state.sessionState.isHydrated)
+  if (!isHydrated) {
+    return <NavStack />
+  }
+
   return <NavStack />
 }
 
